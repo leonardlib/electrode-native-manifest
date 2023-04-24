1cf752aedb2b0a0d9226652bfbfaf04adc284e9f
Leonardo Lira Becerra <leonardolirabecerra@gmail.com>
2023-04-24 13:31:39 -0600

    react-native-webview

--- plugins/ern_v0.49.0+/@react-native-community/checkbox_v0.5.0+/RNCCheckboxPlugin.java
+++ plugins/ern_v0.49.0+/@react-native-community/checkbox_v0.5.0+/RNCCheckboxPlugin.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactInstanceManagerBuilder;
 import com.facebook.react.ReactPackage;
 import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
 
-public class ReactCheckboxPlugin implements ReactPlugin {
+public class RNCCheckboxPlugin implements ReactPlugin {
     public ReactPackage hook(@NonNull Application application,
                       @Nullable ReactPluginConfig config) {
         return new ReactCheckBoxPackage();
